eb2e671df127cbbf54eec4f071d356232c2577f2
achoma <139071702+achoma@users.noreply.github.com>
2024-02-14 15:03:22 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
 
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+//   pierwszy
+let timer = null;
+
+start.addEventListener("click", () => {
+    timer = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    start.disabled = true;
+    stop.disabled = false;
+});
+stop.addEventListener("click", () => {
+    clearInterval(timer); 
+    start.disabled = false;
+    stop.disabled = true;
+});
