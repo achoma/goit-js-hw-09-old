a1fd490314d7e21ed1c8dd37f923ba5eafa2c8ff
achoma <139071702+achoma@users.noreply.github.com>
2024-02-14 14:03:44 +0000

    Deploying to gh-pages from @ achoma/goit-js-hw-09@eb2e671df127cbbf54eec4f071d356232c2577f2 🚀

---
+++ 01-color-switcher.278f1078.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");document.querySelector("body");var n=null;e.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(n),e.disabled=!1,t.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.278f1078.js.map
