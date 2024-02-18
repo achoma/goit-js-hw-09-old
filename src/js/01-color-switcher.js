aa4e9ba7ea8ed899c93e5ac7adb150a90df615ee
achoma <139071702+achoma@users.noreply.github.com>
2024-02-18 01:22:51 +0100

    task2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,7 +5,6 @@ const bodyColor = document.querySelector('body');
 function getRandomHexColor() {
    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
-//   pierwszy
 let timer = null;
 
 start.addEventListener("click", () => {
